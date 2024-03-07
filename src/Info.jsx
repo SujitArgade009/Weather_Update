e36a5b9b882ebfc407417bba587a164717d2fccc
SujitArgade009 <sujitargade009@gmail.com>
2024-03-07 11:13:36 +0530

    New Changes

--- src/Info.jsx
+++ src/Info.jsx
@@ -3,21 +3,36 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import "./Info.css";
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
 
 export default function Info({info}){
-    const IMG_URL="https://images.unsplash.com/photo-1505533321630-975218a5f66f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D"
+   // const IMG_URL="https://images.unsplash.com/photo-1505533321630-975218a5f66f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D";
+    const Rain_URL="https://images.unsplash.com/photo-1534274988757-a28bf1a57c17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGZyZWUlMjByYWlueSUyMHNlYXNvbiUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D";
+    const Cold_URL="https://images.unsplash.com/photo-1528191710846-99b8717a2830?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJlZSUyMGNvbGQlMjBpbWFnZXN8ZW58MHx8MHx8fDA%3D";
+    const Hot_URL="https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGZyZWUlMjBzdW1tZXIlMjBpbWFnZXN8ZW58MHx8MHx8fDA%3D";
     return (
         <div className="InfoBox">
         <div className='CardContainer'>
         <Card sx={{ maxWidth: 345 }}>
         <CardMedia
             sx={{ height: 140 }}
-            image={IMG_URL}
+            image={info.humidity > 80 
+                ? Rain_URL 
+                : info.temp > 15
+                ? Hot_URL 
+                :Cold_URL}
             title="green iguana"
         />
         <CardContent>
             <Typography gutterBottom variant="h5" component="div">
             {info.city}
+                {info.humidity > 80 
+                ? <ThunderstormIcon/> 
+                : info.temp > 15
+                ? <WbSunnyIcon/> 
+                :<AcUnitIcon/>}
             </Typography>
             <Typography variant="body2" color="text.secondary" component={"span"}>
             <p><b>Tempreture: {info.temp}&deg;C</b></p>
--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -2,27 +2,35 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import "./SearchBox.css";
 import { useState } from 'react';
+import { color } from '@mui/system';
 
 export default function SearchBox({updateInfo}){
     let [city, setCity]=useState("Pune");
+    let [err, setError]=useState(false);
+
     const URL="https://api.openweathermap.org/data/2.5/weather";
     const API_KEY="40d17b01aed0da03baba587b36c9ff78";
 
     let getWeatherInfo= async()=>{
-       let res= await fetch(`${URL}?q=${city}&appid=${API_KEY}&units=metric`);
-       let jsonRes=await res.json();
-    //    console.log(jsonRes);
-       let result={
-        city:city,
-        temp: jsonRes.main.temp,
-        mintemp: jsonRes.main.temp_min,
-        maxtemp: jsonRes.main.temp_max,
-        humidity: jsonRes.main.humidity,
-        feels_like: jsonRes.main.feels_like,
-        weather: jsonRes.weather[0].description,
-       }
-       console.log(result);
-       return result;
+        try{
+            let res= await fetch(`${URL}?q=${city}&appid=${API_KEY}&units=metric`);
+            let jsonRes=await res.json();
+         //    console.log(jsonRes);
+            let result={
+             city:city,
+             temp: jsonRes.main.temp,
+             mintemp: jsonRes.main.temp_min,
+             maxtemp: jsonRes.main.temp_max,
+             humidity: jsonRes.main.humidity,
+             feels_like: jsonRes.main.feels_like,
+             weather: jsonRes.weather[0].description,
+            }
+            console.log(result);
+            return result;
+        }catch(err){
+            throw err;
+        }
+      
     };
     let handleChange=(event)=>{
         // console.log(event);
@@ -31,11 +39,16 @@ export default function SearchBox({updateInfo}){
         setCity(event.target.value);
     }
     let handleSubmit= async(event)=>{
-        event.preventDefault();
-        console.log(city);
-        setCity("");
-        let newinfo= await getWeatherInfo();
-        updateInfo(newinfo);
+        try{
+            event.preventDefault();
+            console.log(city);
+            setCity("");
+            let newinfo= await getWeatherInfo();
+            updateInfo(newinfo);
+        }catch(err){
+            setError(true);
+        }
+      
     }
     return(
         <div className='SearchBox'>
@@ -43,6 +56,7 @@ export default function SearchBox({updateInfo}){
             <TextField id="city" label="City Name" variant="outlined" value={city} onChange={handleChange} required/><br></br>
             <br></br>
             <Button variant="contained" type='submit'>Search</Button>
+            {err && <p style={{color:"red"}}>No such Place Exists in Our API</p>}
             </form>
         </div>
     )
